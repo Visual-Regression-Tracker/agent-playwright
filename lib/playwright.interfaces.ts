7e2804ca034a35bcee4790fb086e23c61fee3918
Pavel Strunkin <pashidlos@gmail.com>
2020-09-17 00:38:17 +0200

    Update playwright.interfaces.ts

--- lib/playwright.interfaces.ts
+++ lib/playwright.interfaces.ts
@@ -10,28 +10,6 @@ export interface ElementHandleScreenshotOptions {
   timeout?: number;
 }
 
-export interface PageScreenshotOptions {
-  /**
-   * When true, takes a screenshot of the full scrollable page, instead of the currently visibvle viewport. Defaults to `false`.
-   */
-  fullPage: boolean;
-
-  /**
-   * An object which specifies clipping of the resulting image. Should have the following fields:
-   */
-  clip?: PageScreenshotOptionsClip;
-
-  /**
-   * Hides default white background and allows capturing screenshots with transparency. Not applicable to `jpeg` images. Defaults to `false`.
-   */
-  omitBackground?: boolean;
-
-  /**
-   * Maximum time in milliseconds, defaults to 30 seconds, pass `0` to disable timeout. The default value can be changed by using the browserContext.setDefaultTimeout(timeout) or page.setDefaultTimeout(timeout) methods.
-   */
-  timeout?: number;
-}
-
 export interface PageScreenshotOptionsClip {
   /**
    * x-coordinate of top-left corner of clip area
@@ -54,6 +32,34 @@ export interface PageScreenshotOptionsClip {
   height: number;
 }
 
+export interface PageScreenshotOptions {
+  /**
+   * When true, takes a screenshot of the full scrollable page, instead of the currently visibvle viewport. Defaults to `false`.
+   */
+  fullPage: boolean;
+
+  /**
+   * An object which specifies clipping of the resulting image. Should have the following fields:
+   */
+  clip?: PageScreenshotOptionsClip;
+
+  /**
+   * Hides default white background and allows capturing screenshots with transparency. Not applicable to `jpeg` images. Defaults to `false`.
+   */
+  omitBackground?: boolean;
+
+  /**
+   * Maximum time in milliseconds, defaults to 30 seconds, pass `0` to disable timeout. The default value can be changed by using the browserContext.setDefaultTimeout(timeout) or page.setDefaultTimeout(timeout) methods.
+   */
+  timeout?: number;
+}
+
+export interface Agent {
+  os?: string;
+  device?: string;
+  viewport?: string;
+}
+
 export interface PageTrackOptions {
   diffTollerancePercent?: number;
   screenshotOptions?: PageScreenshotOptions;
@@ -64,10 +70,4 @@ export interface ElementHandleTrackOptions {
   diffTollerancePercent?: number;
   screenshotOptions?: ElementHandleScreenshotOptions;
   agent?: Agent;
-}
-
-export interface Agent {
-  os?: string;
-  device?: string;
-  viewport?: string;
 }
